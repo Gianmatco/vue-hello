ecc5dfc23655e43e62d0f2f462d916f3fe9bd7c1
Gianmatco <gianmarco.culqui.ikaros@gmail.com>
2022-04-21 16:12:49 +0200

    up

---
+++ script.js
@@ -0,0 +1,20 @@
+/*
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.*/ 
+
+const app = new Vue ({
+    el:'#app',
+    data:{
+        title:"il temibile husky",
+        image:"./img/IMG_5583.jpeg"
+
+    },
+    
+    methods:{
+        
+        
+
+    }
+})
